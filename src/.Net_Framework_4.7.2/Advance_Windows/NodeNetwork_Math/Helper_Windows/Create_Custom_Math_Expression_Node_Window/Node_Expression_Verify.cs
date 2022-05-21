1cbe16e77f27d6fde9558f02b6c0bb28cadb884a
Nirav Patel <nirav360n@gmail.com>
2022-05-21 15:20:51 -0400

    Updated NodeEditor Expression Verify

--- src/.Net_Framework_4.7.2/Advance_Windows/NodeNetwork_Math/Helper_Windows/Create_Custom_Math_Expression_Node_Window/Node_Expression_Verify.cs
+++ src/.Net_Framework_4.7.2/Advance_Windows/NodeNetwork_Math/Helper_Windows/Create_Custom_Math_Expression_Node_Window/Node_Expression_Verify.cs
@@ -1,4 +1,5 @@
-﻿using MahApps.Metro.Controls;
+﻿using Custom_Math_Expression_Class;
+using MahApps.Metro.Controls;
 using org.mariuszgromada.math.mxparser;
 using System;
 using System.Windows;
@@ -21,7 +22,7 @@ namespace Create_Custom_Math_Expression_Node
                 {
                     if (Check_if_Expression_contains_Proper_Inputs(Total_Input_Select_Index))
                     {
-                        if (Math_Expression_Verify(Total_Input_Select_Index))
+                        if (Math_Expression_Verify())
                         {
 
                             Expression_Verify_in_Progress = false;
@@ -50,108 +51,120 @@ namespace Create_Custom_Math_Expression_Node
             }
         }
 
-        private bool Math_Expression_Verify(int Total_Inputs)
+        private bool Math_Expression_Verify()
         {
-            double[] Dummy_Data = { 1, 2, 3 };
-            double[] Results = new double[3];
-
-            Argument[] argument = new Argument[Total_Inputs + 1];
-
-            if (Total_Inputs >= 0)
+            if (Fast_Expression_Parsing_Library)
             {
-                argument[0] = new Argument(Input_Text_1, 0);
-            }
-
-            if (Total_Inputs >= 1)
-            {
-                argument[1] = new Argument(Input_Text_2, 0);
-            }
-
-            if (Total_Inputs >= 2)
-            {
-                argument[2] = new Argument(Input_Text_3, 0);
-            }
-
-            if (Total_Inputs >= 3)
-            {
-                argument[3] = new Argument(Input_Text_4, 0);
-            }
-
-            if (Total_Inputs >= 4)
-            {
-                argument[4] = new Argument(Input_Text_5, 0);
-            }
-
-            if (Total_Inputs >= 5)
-            {
-                argument[5] = new Argument(Input_Text_6, 0);
-            }
-
-            if (Total_Inputs >= 6)
-            {
-                argument[6] = new Argument(Input_Text_7, 0);
-            }
-
-            org.mariuszgromada.math.mxparser.Expression expression = new org.mariuszgromada.math.mxparser.Expression(Expression, argument);
-
-            if (expression.checkSyntax())
-            {
-                insert_Log("Input Values: " + Dummy_Data[0] + ", " + Dummy_Data[1] + ", " + Dummy_Data[2], 5);
-                for (int i = 0; i < 3; i++)
+                if (Total_Input_Select_Index <= 3)
                 {
-                    if (Total_Inputs >= 0)
+                    Custom_Math_Expression_Parse Test_Case = null;
+                    switch (Total_Input_Select_Index)
                     {
-                        argument[0].setArgumentValue(Dummy_Data[i]);
+                        case 0:
+                            Test_Case = new MathNET_Symbolics_Expression_Parser(Expression, Output_Text, Input_Text_1);
+                            break;
+                        case 1:
+                            Test_Case = new MathNET_Symbolics_Expression_Parser(Expression, Output_Text, Input_Text_1, Input_Text_2);
+                            break;
+                        case 2:
+                            Test_Case = new MathNET_Symbolics_Expression_Parser(Expression, Output_Text, Input_Text_1, Input_Text_2, Input_Text_3);
+                            break;
+                        case 3:
+                            Test_Case = new MathNET_Symbolics_Expression_Parser(Expression, Output_Text, Input_Text_1, Input_Text_2, Input_Text_3, Input_Text_4);
+                            break;
+                        default:
+                            break;
                     }
-
-                    if (Total_Inputs >= 1)
+                    if (Test_Case != null)
                     {
-                        argument[1].setArgumentValue(Dummy_Data[i]);
+                        (bool isValid, string Message_1, string Message_2, string Message_3) = Test_Case.Verify_Expression();
+                        if (isValid)
+                        {
+                            insert_Log(Message_1, 0);
+                            insert_Log(Message_2, 0);
+                            insert_Log(Message_3, 0);
+                            return true;
+                        }
+                        else
+                        {
+                            insert_Log(Message_1, 1);
+                            insert_Log(Message_2, 1);
+                            insert_Log(Message_3, 1);
+                            return false;
+                        }
                     }
-
-                    if (Total_Inputs >= 2)
+                    else
                     {
-                        argument[2].setArgumentValue(Dummy_Data[i]);
-                    }
-
-                    if (Total_Inputs >= 3)
-                    {
-                        argument[3].setArgumentValue(Dummy_Data[i]);
-                    }
-
-                    if (Total_Inputs >= 4)
-                    {
-                        argument[4].setArgumentValue(Dummy_Data[i]);
-                    }
-
-                    if (Total_Inputs >= 5)
-                    {
-                        argument[5].setArgumentValue(Dummy_Data[i]);
-                    }
-
-                    if (Total_Inputs >= 6)
-                    {
-                        argument[6].setArgumentValue(Dummy_Data[i]);
-                    }
-                    Results[i] = expression.calculate();
-                }
-                for (int i = 0; i < 3; i++)
-                {
-                    if (double.IsNaN(Results[i]) || double.IsInfinity(Results[i]))
-                    {
-                        insert_Log("Output Values were not valid real numbers.", 1);
+                        insert_Log("Test case was null, this should not have happened, check source code.", 1);
                         return false;
                     }
                 }
-                insert_Log("Output Values: " + Results[0] + ", " + Results[1] + ", " + Results[2], 5);
-                insert_Log("Expression Syntax Check Passed.", 0);
-                return true;
+                else
+                {
+                    insert_Log("MathNET Symbolics Expression Parser only supports a maximum of 4 inputs.", 1);
+                    return false;
+                }
             }
             else
             {
-                string Expression_Error_Message = expression.getErrorMessage();
-                insert_Log(Expression_Error_Message, 1);
-                return false;
+                if (Total_Input_Select_Index <= 6)
+                {
+                    Custom_Math_Expression_Parse Test_Case = null;
+                    switch (Total_Input_Select_Index)
+                    {
+                        case 0:
+                            Test_Case = new mXparser_Expression_Parser(Expression, Output_Text, Input_Text_1);
+                            break;
+                        case 1:
+                            Test_Case = new mXparser_Expression_Parser(Expression, Output_Text, Input_Text_1, Input_Text_2);
+                            break;
+                        case 2:
+                            Test_Case = new mXparser_Expression_Parser(Expression, Output_Text, Input_Text_1, Input_Text_2, Input_Text_3);
+                            break;
+                        case 3:
+                            Test_Case = new mXparser_Expression_Parser(Expression, Output_Text, Input_Text_1, Input_Text_2, Input_Text_3, Input_Text_4);
+                            break;
+                        case 4:
+                            Test_Case = new mXparser_Expression_Parser(Expression, Output_Text, Input_Text_1, Input_Text_2, Input_Text_3, Input_Text_4, Input_Text_5);
+                            break;
+                        case 5:
+                            Test_Case = new mXparser_Expression_Parser(Expression, Output_Text, Input_Text_1, Input_Text_2, Input_Text_3, Input_Text_4, Input_Text_5, Input_Text_6);
+                            break;
+                        case 6:
+                            Test_Case = new mXparser_Expression_Parser(Expression, Output_Text, Input_Text_1, Input_Text_2, Input_Text_3, Input_Text_4, Input_Text_5, Input_Text_6, Input_Text_7);
+                            break;
+                        default:
+                            break;
+                    }
+                    if (Test_Case != null)
+                    {
+                        (bool isValid, string Message_1, string Message_2, string Message_3) = Test_Case.Verify_Expression();
+                        if (isValid)
+                        {
+                            insert_Log(Message_1, 0);
+                            insert_Log(Message_2, 0);
+                            insert_Log(Message_3, 0);
+                            return true;
+                        }
+                        else
+                        {
+                            insert_Log(Message_1, 1);
+                            insert_Log(Message_2, 1);
+                            insert_Log(Message_3, 1);
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        insert_Log("Test case was null, this should not have happened, check source code.", 1);
+                        return false;
+                    }
+                }
+                else
+                {
+                    insert_Log("mXparser Expression Parser only supports a maximum of 7 inputs.", 1);
+                    return false;
+                }
             }
         }
     }
